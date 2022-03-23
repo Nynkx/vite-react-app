88eebb615e18631b0d30619d1dd875c52c5b90c4
unknown <conghuy649@gmail.com>
2022-03-23 16:50:19 +0700

    a

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,7 +3,22 @@ import { FC } from "react";
 
 import { Grid, Container } from "@mantine/core";
 
+interface Test {
+  prop1: string;
+  prop2: string;
+}
+
+type InterfaceToKV<T> = {
+  [K in keyof T]: { key: K; value: T[K] };
+}[keyof T];
+
 const Home: FC = function () {
+  var testvar: Test = {
+    prop1: "value1",
+    prop2: "value2",
+  };
+  type t = InterfaceToKV<Test>;
+
   return (
     <>
       <Grid>
